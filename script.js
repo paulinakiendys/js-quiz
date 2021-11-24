13bfd333c9336759aab17aa1673e39fca999d5ef
Paulina Kiendys <kiendyspaulina@gmail.comgit config --listgit config --global user.email kiendyspaulina@gmail.com>
2021-11-24 14:14:56 +0100

    Create options and render to the DOM.

--- script.js
+++ script.js
@@ -1,163 +1,166 @@
 const students = [
-	{
-		"name" : "Adi Dzocaj",
-		"image": "assets/images/students/adi-dzocaj.jpg",
-	},
-	{
-		"name" : "Alexander Bergquist",
-		"image": "assets/images/students/alexander-bergquist.jpg",
-	},
-	{
-		"name" : "Alexander Kocman",
-		"image": "assets/images/students/alexander-kocman.jpg",
-	},
-	{
-		"name" : "Benjamin Benson",
-		"image": "assets/images/students/benjamin-benson.jpg",
-	},
-	{
-		"name" : "Benjamin Tsubarah",
-		"image": "assets/images/students/benjamin-tsubarah.jpg",
-	},
-	{
-		"name" : "Calle Nilsson",
-		"image": "assets/images/students/calle-nilsson.jpg",
-	},
-	{
-		"name" : "Chikage Takahashi Molander",
-		"image": "assets/images/students/chikage-takahashi-molander.jpg",
-	},
-	{
-		"name" : "Daniel Be",
-		"image": "assets/images/students/daniel-be.jpg",
-	},
-	{
-		"name" : "Daniel Carlsson",
-		"image": "assets/images/students/daniel-carlsson.jpg",
-	},
-	{
-		"name" : "Elin Ahlgren",
-		"image": "assets/images/students/elin-ahlgren.jpg",
-	},
-	{
-		"name" : "Emma Käck",
-		"image": "assets/images/students/emma-kack.jpg",
-	},
-	{
-		"name" : "Eric Ståhl",
-		"image": "assets/images/students/eric-stahl.jpg",
-	},
-	{
-		"name" : "Frans Gustavson Påsse",
-		"image": "assets/images/students/frans-gustavson-passe.jpg",
-	},
-	{
-		"name" : "Glafira Veretennikova",
-		"image": "assets/images/students/glafira-veretennikova.jpg",
-	},
-	{
-		"name" : "Gustaf Grönlund",
-		"image": "assets/images/students/gustaf-gronlund.jpg",
-	},
-	{
-		"name" : "Hanna Håkanson",
-		"image": "assets/images/students/hanna-hakanson.jpg",
-	},
-	{
-		"name" : "Heidi Sjöberg",
-		"image": "assets/images/students/heidi-sjoberg.jpg",
-	},
-	{
-		"name" : "Hugo Carzborn",
-		"image": "assets/images/students/hugo-carzborn.jpg",
-	},
-	{
-		"name" : "Jesper Kling",
-		"image": "assets/images/students/jesper-kling.jpg",
-	},
-	{
-		"name" : "Johan Ranestam",
-		"image": "assets/images/students/johan-ranestam.jpg",
-	},
-	{
-		"name" : "Johanna Bäckström",
-		"image": "assets/images/students/johanna-backstrom.jpg",
-	},
-	{
-		"name" : "Johanna Jönsson",
-		"image": "assets/images/students/johanna-jonsson.jpg",
-	},
-	{
-		"name" : "Jona Torsson",
-		"image": "assets/images/students/jona-torsson.jpg",
-	},
-	{
-		"name" : "Josefine Ahlstedt",
-		"image": "assets/images/students/josefine-ahlstedt.jpg",
-	},
-	{
-		"name" : "Julia Jespersdotter Högman",
-		"image": "assets/images/students/julia-jespersdotter-hogman.jpg",
-	},
-	{
-		"name" : "Julia Nemell",
-		"image": "assets/images/students/julia-nemell.jpg",
-	},
-	{
-		"name" : "Linus Lindberg",
-		"image": "assets/images/students/linus-lindberg.jpg",
-	},
-	{
-		"name" : "Malin Olsson",
-		"image": "assets/images/students/malin-olsson.jpg",
-	},
-	{
-		"name" : "Maria Haara-Lundhammar",
-		"image": "assets/images/students/maria-haara-lundhammar.jpg",
-	},
-	{
-		"name" : "Maria Lövgren",
-		"image": "assets/images/students/maria-lovgren.jpg",
-	},
-	{
-		"name" : "Nikola Dimitrijoski",
-		"image": "assets/images/students/nikola-dimitrijoski.jpg",
-	},
-	{
-		"name" : "Paulina Kiendys",
-		"image": "assets/images/students/paulina-kiendys.jpg",
-	},
-	{
-		"name" : "Raymond Lam",
-		"image": "assets/images/students/raymond-lam.jpg",
-	},
-	{
-		"name" : "Robin Karlsson",
-		"image": "assets/images/students/robin-karlsson.jpg",
-	},
-	{
-		"name" : "Sara Almqvist",
-		"image": "assets/images/students/sara-almqvist.jpg",
-	},
-	{
-		"name" : "Tim Nilsson",
-		"image": "assets/images/students/tim-nilsson.jpg",
-	},
-	{
-		"name" : "Tirapat Sukjit",
-		"image": "assets/images/students/tirapat-sukjit.jpg",
-	},
-	{
-		"name" : "Tobias Silfverberg",
-		"image": "assets/images/students/tobias-silfverberg.jpg",
-	},
-	{
-		"name" : "Wiktoria Dobrzewinska",
-		"image": "assets/images/students/wiktoria-dobrzewinska.jpg",
-	},
+    {
+        "name" : "Adi Dzocaj",
+        "image": "assets/images/students/adi-dzocaj.jpg",
+    },
+    {
+        "name" : "Alexander Bergquist",
+        "image": "assets/images/students/alexander-bergquist.jpg",
+    },
+    {
+        "name" : "Alexander Kocman",
+        "image": "assets/images/students/alexander-kocman.jpg",
+    },
+    {
+        "name" : "Benjamin Benson",
+        "image": "assets/images/students/benjamin-benson.jpg",
+    },
+    {
+        "name" : "Benjamin Tsubarah",
+        "image": "assets/images/students/benjamin-tsubarah.jpg",
+    },
+    {
+        "name" : "Calle Nilsson",
+        "image": "assets/images/students/calle-nilsson.jpg",
+    },
+    {
+        "name" : "Chikage Takahashi Molander",
+        "image": "assets/images/students/chikage-takahashi-molander.jpg",
+    },
+    {
+        "name" : "Daniel Be",
+        "image": "assets/images/students/daniel-be.jpg",
+    },
+    {
+        "name" : "Daniel Carlsson",
+        "image": "assets/images/students/daniel-carlsson.jpg",
+    },
+    {
+        "name" : "Elin Ahlgren",
+        "image": "assets/images/students/elin-ahlgren.jpg",
+    },
+    {
+        "name" : "Emma Käck",
+        "image": "assets/images/students/emma-kack.jpg",
+    },
+    {
+        "name" : "Eric Ståhl",
+        "image": "assets/images/students/eric-stahl.jpg",
+    },
+    {
+        "name" : "Frans Gustavson Påsse",
+        "image": "assets/images/students/frans-gustavson-passe.jpg",
+    },
+    {
+        "name" : "Glafira Veretennikova",
+        "image": "assets/images/students/glafira-veretennikova.jpg",
+    },
+    {
+        "name" : "Gustaf Grönlund",
+        "image": "assets/images/students/gustaf-gronlund.jpg",
+    },
+    {
+        "name" : "Hanna Håkanson",
+        "image": "assets/images/students/hanna-hakanson.jpg",
+    },
+    {
+        "name" : "Heidi Sjöberg",
+        "image": "assets/images/students/heidi-sjoberg.jpg",
+    },
+    {
+        "name" : "Hugo Carzborn",
+        "image": "assets/images/students/hugo-carzborn.jpg",
+    },
+    {
+        "name" : "Jesper Kling",
+        "image": "assets/images/students/jesper-kling.jpg",
+    },
+    {
+        "name" : "Johan Ranestam",
+        "image": "assets/images/students/johan-ranestam.jpg",
+    },
+    {
+        "name" : "Johanna Bäckström",
+        "image": "assets/images/students/johanna-backstrom.jpg",
+    },
+    {
+        "name" : "Johanna Jönsson",
+        "image": "assets/images/students/johanna-jonsson.jpg",
+    },
+    {
+        "name" : "Jona Torsson",
+        "image": "assets/images/students/jona-torsson.jpg",
+    },
+    {
+        "name" : "Josefine Ahlstedt",
+        "image": "assets/images/students/josefine-ahlstedt.jpg",
+    },
+    {
+        "name" : "Julia Jespersdotter Högman",
+        "image": "assets/images/students/julia-jespersdotter-hogman.jpg",
+    },
+    {
+        "name" : "Julia Nemell",
+        "image": "assets/images/students/julia-nemell.jpg",
+    },
+    {
+        "name" : "Linus Lindberg",
+        "image": "assets/images/students/linus-lindberg.jpg",
+    },
+    {
+        "name" : "Malin Olsson",
+        "image": "assets/images/students/malin-olsson.jpg",
+    },
+    {
+        "name" : "Maria Haara-Lundhammar",
+        "image": "assets/images/students/maria-haara-lundhammar.jpg",
+    },
+    {
+        "name" : "Maria Lövgren",
+        "image": "assets/images/students/maria-lovgren.jpg",
+    },
+    {
+        "name" : "Nikola Dimitrijoski",
+        "image": "assets/images/students/nikola-dimitrijoski.jpg",
+    },
+    {
+        "name" : "Paulina Kiendys",
+        "image": "assets/images/students/paulina-kiendys.jpg",
+    },
+    {
+        "name" : "Raymond Lam",
+        "image": "assets/images/students/raymond-lam.jpg",
+    },
+    {
+        "name" : "Robin Karlsson",
+        "image": "assets/images/students/robin-karlsson.jpg",
+    },
+    {
+        "name" : "Sara Almqvist",
+        "image": "assets/images/students/sara-almqvist.jpg",
+    },
+    {
+        "name" : "Tim Nilsson",
+        "image": "assets/images/students/tim-nilsson.jpg",
+    },
+    {
+        "name" : "Tirapat Sukjit",
+        "image": "assets/images/students/tirapat-sukjit.jpg",
+    },
+    {
+        "name" : "Tobias Silfverberg",
+        "image": "assets/images/students/tobias-silfverberg.jpg",
+    },
+    {
+        "name" : "Wiktoria Dobrzewinska",
+        "image": "assets/images/students/wiktoria-dobrzewinska.jpg",
+    },
 ];
 
 let numberOfQuestions = 8;
+let studentsSelection = [];
+let options = [];
+let correctName = '';
 
 const questionsWrapperEl = document.querySelector('.questions-wrapper');
 
@@ -177,17 +180,42 @@ const getRandomStudents = (array) => {
 };
 
 const renderQuestions = (array) => {
-    questionsWrapperEl.innerHTML = array.map((student) => `
+    questionsWrapperEl.innerHTML = array.map((student, index) => `
         <div id="question" class="col-sm-12 col-md-6 col-lg-3 p-2">
             <div class="border rounded">
                 <div class="image-wrapper p-4">
                     <img src="${student.image}" class="img-fluid rounded-circle" alt="${student.name}">
+                </div>
+                <div class="options-wrapper bg-secondary border rounded p-2 m-3 ">
+                ${(student.options).map(option => `
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="questionIndex${index}" id="${option}" value="${option}" autocomplete="off">
+                        <label class="form-check-label" for="${option}}">${option}</label>
+                    </div>
+                    `).join('')} 
                 </div>
             </div>
         </div>
     `).join('');
 };
 
-const studentsSelection = getRandomStudents(students);
+const addRandomizedOptions = (array) =>
+    array.forEach(element => {
+        correctName = element.name;
+        const filtered = students.filter(student => {
+            return student.name !== correctName;
+        });
+        shuffleArray(filtered);
+        options = filtered
+            .slice(0, 3)
+            .map(student => student.name);
+        options.push(correctName);
+        shuffleArray(options);
+        element.options = options;
+    });
+
+studentsSelection = getRandomStudents(students);
+
+addRandomizedOptions(studentsSelection);
 
 renderQuestions(studentsSelection);
